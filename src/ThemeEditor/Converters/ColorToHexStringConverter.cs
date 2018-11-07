809e9c3860f02e335b7cdca24e20ee2dd918a0c1
Wiesław Šoltés <wieslaw.soltes@gmail.com>
2018-11-07 20:18:52 +0100

    Display color string

---
+++ src/ThemeEditor/Converters/ColorToHexStringConverter.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Globalization;
+using Avalonia;
+using Avalonia.Data.Converters;
+using Avalonia.Media;
+
+namespace ThemeEditor.Converters
+{
+    public class ColorToHexStringConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (value is Color color && targetType == typeof(string))
+            {
+                return $"#{color.A.ToString("X2")}{color.R.ToString("X2")}{color.G.ToString("X2")}{color.B.ToString("X2")}";
+            }
+            return AvaloniaProperty.UnsetValue;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (value is string s && targetType == typeof(Color))
+            {
+                return Color.Parse(s);
+            }
+            return AvaloniaProperty.UnsetValue;
+        }
+    }
+}
