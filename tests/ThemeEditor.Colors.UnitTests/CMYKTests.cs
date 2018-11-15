c89a692b5996270daa8a7d39e541f53fbb283356
wieslawsoltes <wieslaw.soltes@gmail.com>
2018-11-15 12:25:25 +0000

    Fix variable name

--- tests/ThemeEditor.Colors.UnitTests/CMYKTests.cs
+++ tests/ThemeEditor.Colors.UnitTests/CMYKTests.cs
@@ -8,7 +8,7 @@ namespace ThemeEditor.Colors.UnitTests
         public void CMYK_ToRGB()
         {
             var cmyk = new CMYK(46.4, 25.6, 0, 50.98);
-            var actual = rgb.ToRGB();
+            var actual = cmyk.ToRGB();
             Assert.Equal(67, actual.R);
             Assert.Equal(93, actual.G);
             Assert.Equal(125, actual.B);
@@ -18,7 +18,7 @@ namespace ThemeEditor.Colors.UnitTests
         public void CMYK_ToHSV()
         {
             var cmyk = new CMYK(46.4, 25.6, 0, 50.98);
-            var actual = rgb.ToHSV();
+            var actual = cmyk.ToHSV();
             Assert.Equal(213.1, actual.H, 2);
             Assert.Equal(46.4, actual.S, 2);
             Assert.Equal(49.02, actual.V, 2);
