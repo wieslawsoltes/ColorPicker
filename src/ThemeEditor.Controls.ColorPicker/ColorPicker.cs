47facc80d5c813da822c8f31dbff3b37ffbd0743
wieslawsoltes <wieslaw.soltes@gmail.com>
2018-11-15 08:35:46 +0000

    Added missing using

--- src/ThemeEditor.Controls.ColorPicker/ColorPicker.cs
+++ src/ThemeEditor.Controls.ColorPicker/ColorPicker.cs
@@ -4,6 +4,7 @@ using Avalonia.Controls;
 using Avalonia.Controls.Primitives;
 using Avalonia.Input;
 using Avalonia.Media;
+using ThemeEditor.Controls.ColorPicker.Converters;
 
 namespace ThemeEditor.Controls.ColorPicker
 {
