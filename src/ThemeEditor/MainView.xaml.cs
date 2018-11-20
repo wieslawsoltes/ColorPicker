94f0a155e6956c6e8cfd20babc87187c1ee13ea3
wieslawsoltes <wieslaw.soltes@gmail.com>
2018-11-20 11:18:43 +0000

    Try code behind

--- src/ThemeEditor/MainView.xaml.cs
+++ src/ThemeEditor/MainView.xaml.cs
@@ -15,6 +15,9 @@ namespace ThemeEditor
         private ThemeEditorView _editorView = null;
         private ColorPicker _colorPicker = null;
         private ColorBlender _colorBlender = null;
+        private DropDown _themesSelector = null;
+        private StyleInclude _lightTheme = null;
+        private StyleInclude _darkTheme = null;
 
         public MainView()
         {
@@ -25,6 +28,10 @@ namespace ThemeEditor
             _colorPicker = this.Find<ColorPicker>("colorPicker");
             _colorBlender = this.Find<ColorBlender>("colorBlender");
             _colorBlender.DataContext = new ColorMatchViewModel(199, 95, 62);
+            _themeSelector = this.Find<DropDown>("themeSelector");
+            _themeSelector.SelectionChanged += ThemeSelectionChanged;
+            _lightTheme = AvaloniaXamlLoader.Parse<StyleInclude>(@"<StyleInclude xmlns='https://github.com/avaloniaui' Source='resm:ThemeEditor.Themes.BaseLight.xaml?assembly=ThemeEditor'/>");
+            _darkTheme = AvaloniaXamlLoader.Parse<StyleInclude>(@"<StyleInclude xmlns='https://github.com/avaloniaui' Source='resm:ThemeEditor.Themes.BaseDark.xaml?assembly=ThemeEditor'/>");
         }
 
         private void InitializeComponent()
@@ -49,6 +56,19 @@ namespace ThemeEditor
             if (DataContext is ThemeEditorViewModel editor)
             {
                 editor.Detach();
+            }
+        }
+
+        private void ThemeSelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            switch (_themeSelector.SelectedIndex)
+            {
+                case 0:
+                    this.Styles[0]  = _lightTheme;
+                    break;
+                case 1:
+                    this.Styles[0]  = _darkTheme;
+                    break;
             }
         }
     }
