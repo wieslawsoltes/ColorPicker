80568998cb2503c78021b8d8cce6f74158653b1b
Wiesław Šoltés <wieslaw.soltes@gmail.com>
2018-11-22 18:58:51 +0100

    Update MainView.xaml.cs

--- src/ThemeEditor/MainView.xaml.cs
+++ src/ThemeEditor/MainView.xaml.cs
@@ -30,12 +30,22 @@ namespace ThemeEditor
             _colorBlender = this.Find<ColorBlender>("colorBlender");
             _colorBlender.DataContext = new ColorMatchViewModel(199, 95, 62);
             _themeSelector = this.Find<DropDown>("themeSelector");
-            _themeSelector.SelectionChanged += ThemeSelectionChanged;
+            _themeSelector.SelectionChanged += (sender, e) =>
+            {
+                switch (_themeSelector.SelectedIndex)
+                {
+                    case 0:
+                        Styles[0] = _lightTheme;
+                        break;
+                    case 1:
+                        Styles[0] = _darkTheme;
+                        break;
+                }
+            };
             _lightTheme = AvaloniaXamlLoader.Parse<StyleInclude>(@"<StyleInclude xmlns='https://github.com/avaloniaui' Source='resm:Avalonia.Themes.Default.Accents.BaseLight.xaml?assembly=Avalonia.Themes.Default'/>");
             _darkTheme = AvaloniaXamlLoader.Parse<StyleInclude>(@"<StyleInclude xmlns='https://github.com/avaloniaui' Source='resm:Avalonia.Themes.Default.Accents.BaseDark.xaml?assembly=Avalonia.Themes.Default'/>");
             Styles.Add(_darkTheme);
         }
-
         private void InitializeComponent()
         {
             AvaloniaXamlLoader.Load(this);
@@ -61,19 +71,6 @@ namespace ThemeEditor
             if (DataContext is ThemeEditorViewModel editor)
             {
                 editor.Detach();
-            }
-        }
-
-        private void ThemeSelectionChanged(object sender, SelectionChangedEventArgs e)
-        {
-            switch (_themeSelector.SelectedIndex)
-            {
-                case 0:
-                    Styles[0]  = _lightTheme;
-                    break;
-                case 1:
-                    Styles[0]  = _darkTheme;
-                    break;
             }
         }
     }
