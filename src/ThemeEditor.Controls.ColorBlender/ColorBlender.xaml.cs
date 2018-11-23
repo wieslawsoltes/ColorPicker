38cda91e1d55bc9d94b61c801f6cf0f84e5b2842
wieslawsoltes <wieslaw.soltes@gmail.com>
2018-11-23 13:42:04 +0000

    Updated control

--- src/ThemeEditor.Controls.ColorBlender/ColorBlender.xaml.cs
+++ src/ThemeEditor.Controls.ColorBlender/ColorBlender.xaml.cs
@@ -11,6 +11,42 @@ using ThemeEditor.Colors;
 
 namespace ThemeEditor.Controls.ColorBlender
 {
+    internal static class ColorHelpers
+    {
+        public static RGB ToRGB(this Color c)
+        {
+            return new RGB(c.R, c.G, c.B);
+        }
+
+        public static HSV ToHSV(this Color c)
+        {
+            return ToRGB(c).ToHSV();
+        }
+
+        public static Color ToColor(this HSV hsv)
+        {
+            return ToColor(hsv.ToRGB());
+        }
+
+        public static Color ToColor(this RGB rgb)
+        {
+            return Color.FromRgb(
+                (byte)Math.Round(rgb.R),
+                (byte)Math.Round(rgb.G),
+                (byte)Math.Round(rgb.B));
+        }
+
+        public static SolidColorBrush ToSolidColorBrush(this RGB rgb)
+        {
+            return new SolidColorBrush(ToColor(rgb));
+        }
+
+        public static SolidColorBrush ToSolidColorBrush(this HSV hsv)
+        {
+            return new SolidColorBrush(ToColor(hsv));
+        }
+    }
+
     public partial class ColorBlender : UserControl
     {
         private DropDown _algorithm;
