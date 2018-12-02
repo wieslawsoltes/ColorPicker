1cabe6915d3a26b59b8e37f9d1202673326eb489
wieslawsoltes <wieslaw.soltes@gmail.com>
2018-12-02 10:02:04 +0000

    Fix error

--- build/build/Build.cs
+++ build/build/Build.cs
@@ -110,6 +110,6 @@ class Build : NukeBuild
                 .SetVersionSuffix(VersionSuffix)
                 .SetFramework(PublishFramework)
                 .SetRuntime(PublishRuntime)
-                .SetOutput(ArtifactsDirectory / PublishProject + "-" + Runtime));
+                .SetOutput(ArtifactsDirectory / PublishProject + "-" + PublishRuntime));
         });
 }
