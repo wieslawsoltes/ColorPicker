3e9d0e17ad80d06e77bea6aa3ace1f292c3c1b05
wieslawsoltes <wieslaw.soltes@gmail.com>
2018-12-02 09:21:13 +0000

    Make executable

--- build.ps1
+++ build.ps1
@@ -14,7 +14,7 @@ $PSScriptRoot = Split-Path $MyInvocation.MyCommand.Path -Parent
 # CONFIGURATION
 ###########################################################################
 
-$BuildProjectFile = "$PSScriptRoot\build\_build.csproj"
+$BuildProjectFile = "$PSScriptRoot\build\build\_build.csproj"
 $TempDirectory = "$PSScriptRoot\\.tmp"
 
 $DotNetGlobalFile = "$PSScriptRoot\\global.json"
