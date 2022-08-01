934a1cae94ab36c7c6892525b5253f30572c3582
Wiesław Šoltés <wieslawsoltes@users.noreply.github.com>
2022-08-01 09:33:08 +0200

    Add binding sample

--- samples/ColorPickerDemo/MainWindow.axaml.cs
+++ samples/ColorPickerDemo/MainWindow.axaml.cs
@@ -1,13 +1,29 @@
+using Avalonia;
 using Avalonia.Controls;
 using Avalonia.Markup.Xaml;
+using Avalonia.Media;
 
 namespace ColorPickerDemo
 {
     public partial class MainWindow : Window
     {
+        public static readonly StyledProperty<Color?> ColorProperty =
+            AvaloniaProperty.Register<MainWindow, Color?>(nameof(Color));
+
+        public Color? Color
+        {
+            get => GetValue(ColorProperty);
+            set => SetValue(ColorProperty, value);
+        }
+        
         public MainWindow()
         {
             InitializeComponent();
+            
+            Color = Avalonia.Media.Color.FromArgb(0x5F, 0x1F, 0x00, 0x1F);
+            // Color = null;
+
+            DataContext = this;
         }
 
         private void InitializeComponent()
