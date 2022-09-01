595f9092beff0626c9698ff41d4de00d212b81c4
Wiesław Šoltés <wieslaw.soltes@gmail.com>
2022-09-01 09:45:39 +0200

    Update MainWindow.axaml.cs

--- samples/ColorPickerDemo/MainWindow.axaml.cs
+++ samples/ColorPickerDemo/MainWindow.axaml.cs
@@ -64,7 +64,7 @@ public partial class MainWindow : Window
     public MainWindow()
     {
         InitializeComponent();
-            
+
         Color = Avalonia.Media.Color.FromArgb(0x5F, 0x1F, 0x00, 0x1F);
         NullColor = null;
         Hex = "#5F1F001F";
