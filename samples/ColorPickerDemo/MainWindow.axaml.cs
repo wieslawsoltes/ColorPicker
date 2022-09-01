cc7139f750a15aa53d2642a00120da8b042a5463
Wiesław Šoltés <wieslaw.soltes@gmail.com>
2022-09-01 09:13:04 +0200

    Update

--- samples/ColorPickerDemo/MainWindow.axaml.cs
+++ samples/ColorPickerDemo/MainWindow.axaml.cs
@@ -3,32 +3,31 @@ using Avalonia.Controls;
 using Avalonia.Markup.Xaml;
 using Avalonia.Media;
 
-namespace ColorPickerDemo
+namespace ColorPickerDemo;
+
+public partial class MainWindow : Window
 {
-    public partial class MainWindow : Window
+    public static readonly StyledProperty<Color?> ColorProperty =
+        AvaloniaProperty.Register<MainWindow, Color?>(nameof(Color));
+
+    public Color? Color
     {
-        public static readonly StyledProperty<Color?> ColorProperty =
-            AvaloniaProperty.Register<MainWindow, Color?>(nameof(Color));
-
-        public Color? Color
-        {
-            get => GetValue(ColorProperty);
-            set => SetValue(ColorProperty, value);
-        }
+        get => GetValue(ColorProperty);
+        set => SetValue(ColorProperty, value);
+    }
         
-        public MainWindow()
-        {
-            InitializeComponent();
+    public MainWindow()
+    {
+        InitializeComponent();
             
-            Color = Avalonia.Media.Color.FromArgb(0x5F, 0x1F, 0x00, 0x1F);
-            // Color = null;
+        Color = Avalonia.Media.Color.FromArgb(0x5F, 0x1F, 0x00, 0x1F);
+        // Color = null;
 
-            DataContext = this;
-        }
+        DataContext = this;
+    }
 
-        private void InitializeComponent()
-        {
-            AvaloniaXamlLoader.Load(this);
-        }
+    private void InitializeComponent()
+    {
+        AvaloniaXamlLoader.Load(this);
     }
 }
