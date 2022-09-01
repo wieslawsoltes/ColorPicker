beee3ba8a9c5aed0efd2e83ee1fd3ed4df431963
Wiesław Šoltés <wieslaw.soltes@gmail.com>
2022-09-01 09:37:41 +0200

    Create HexToColorConverter.cs

---
+++ samples/ColorPickerDemo/HexToColorConverter.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Globalization;
+using Avalonia;
+using Avalonia.Data.Converters;
+using Avalonia.Media;
+
+namespace ColorPickerDemo;
+
+public class HexToColorConverter : IValueConverter
+{
+    public static HexToColorConverter Instance = new();
+
+    public object? Convert(object? value, Type targetType, object? parameter, CultureInfo culture)
+    {
+        if (value is string s && (targetType == typeof(Color?) || targetType == typeof(Color)))
+        {
+            try
+            {
+                if (Color.TryParse(s, out var color))
+                {
+                    return color;
+                }
+            }
+            catch (Exception)
+            {
+                return AvaloniaProperty.UnsetValue;
+            }
+        }
+        return AvaloniaProperty.UnsetValue;
+    }
+
+    public object? ConvertBack(object? value, Type targetType, object? parameter, CultureInfo culture)
+    {
+        if (value is Color c && targetType == typeof(string))
+        {
+            try
+            {
+                return $"#{c.ToUint32():x8}";
+            }
+            catch (Exception)
+            {
+                return AvaloniaProperty.UnsetValue;
+            }
+        }
+        return AvaloniaProperty.UnsetValue;
+    }
+}
